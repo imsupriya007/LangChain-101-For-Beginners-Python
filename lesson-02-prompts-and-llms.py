463d7a0f067092c5fa8b64450adf24ea7227d3c4
Supriya Raman Singh <54034994+imsupriya007@users.noreply.github.com>
2024-05-20 22:38:21 +0530

    Update lesson-02-prompts-and-llms.py

--- lesson-02-prompts-and-llms.py
+++ lesson-02-prompts-and-llms.py
@@ -8,7 +8,7 @@ from langchain.llms import OpenAI
 llm = OpenAI(temperature=0)
 prompt = "What would a good company name be for a company that makes colorful socks?"
 
-#result = llm.generate(prompt)
+#print(llm(prompt))
 result = llm.generate([prompt] * 5)
 for company_name in result.generations:
     print(company_name[0].text)
