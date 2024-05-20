3d9e82e29ea7194e6ba59a129bb51adf9ff52e12
Supriya Raman Singh <54034994+imsupriya007@users.noreply.github.com>
2024-05-20 23:40:04 +0530

    Update lesson-05-simple-sequential-chains.py

--- lesson-05-simple-sequential-chains.py
+++ lesson-05-simple-sequential-chains.py
@@ -4,9 +4,10 @@ import os
 os.environ["OPENAI_API_KEY"] = "..."
 
 from langchain import OpenAI, LLMChain, PromptTemplate
-from langchain.chains import SimpleSequentialChain
+from langchain.chains import SimpleSequentialChain #Output of one chain is input to another
 
 llm = OpenAI(temperature=0)
+#llm= HuggingFaceHub(repo_id="google/flan-t5-base",model_kwargs={"temperature":1, "max_length":64})
 template = "What is a good name for a company that makes {product}?"
 first_prompt = PromptTemplate.from_template(template)
 first_chain = LLMChain(llm=llm, prompt=first_prompt)
@@ -15,6 +16,6 @@ second_template = "Write a catch phrase for the following company: {company_name
 second_prompt = PromptTemplate.from_template(second_template)
 second_chain = LLMChain(llm=llm, prompt=second_prompt)
 
-overall_chain = SimpleSequentialChain(chains=[first_chain, second_chain], verbose=True)
+overall_chain = SimpleSequentialChain(chains=[first_chain, second_chain], verbose=True) #how chain performs at each call - verbose monitors
 catchphrase = overall_chain.run("colorful socks")
 print(catchphrase)
