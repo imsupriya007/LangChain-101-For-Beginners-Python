72d789d1c4e1877a22aaef2308f1c5bfce91f731
Supriya Raman Singh <54034994+imsupriya007@users.noreply.github.com>
2024-05-21 00:30:24 +0530

    Update lesson-06-human-as-a-tool.py

--- lesson-06-human-as-a-tool.py
+++ lesson-06-human-as-a-tool.py
@@ -8,7 +8,7 @@ from langchain.llms import OpenAI
 from langchain.agents import load_tools, initialize_agent
 from langchain.agents import AgentType
 
-#offer Human as a tool to Agent to ask anything personal/ human required as input
+#offer Human as a tool to Agent to ask anything personal/ human required as input - Agent will wait for input from user
 
 llm = ChatOpenAI(temperature=0.0)
 math_llm = OpenAI(temperature=0.0)
