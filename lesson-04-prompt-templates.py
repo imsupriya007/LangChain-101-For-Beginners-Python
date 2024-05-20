16d474fe486e8f42b465144981a976c4caa6528c
Supriya Raman Singh <54034994+imsupriya007@users.noreply.github.com>
2024-05-20 23:26:32 +0530

    Update lesson-04-prompt-templates.py

--- lesson-04-prompt-templates.py
+++ lesson-04-prompt-templates.py
@@ -3,9 +3,9 @@ import os
 
 os.environ["OPENAI_API_KEY"] = "..."
 
-from langchain import PromptTemplate
+from langchain import PromptTemplate #Modular way to create a prompt and replace with variable values 
 from langchain.llms import OpenAI
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain #take template and chain it with LLM, LLMChain are simplest chain in LangChain
 
 template = "You are a naming consultant for new companies. What is a good name for a {company} that makes {product}?"
 
